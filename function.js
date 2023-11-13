8798b1d43d5e14f2452aec923bacb0090b558ec0
Dr. K.G.B <94070857+kgbcyber@users.noreply.github.com>
2023-11-13 22:51:02 +0100

    UI changes

---
+++ function.js
@@ -0,0 +1,15 @@
+var countDownDate = new Date("Sept 23, 2024 00:00:00").getTime();
+var variable = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(variable);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
